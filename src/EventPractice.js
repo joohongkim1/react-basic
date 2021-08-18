bc1d24221e853f19ab045923a63084123c2ec585
joohongkim1 <wnghd9938@naver.com>
2021-08-18 21:59:41 +0900

    Event Handling with functional component

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,67 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class EventPractice extends Component {
-  state = {
-    message: '',
+const EventPractice = () => {
+  const [form, setForm] = useState({
     username: '',
-  };
-
-  constructor(props) {
-    super(props);
-
-    // 함수 호출 시 메서드와 this 의 관계가 끊어짐
-    // this 를 컴포넌트 자신으로 가리키기 위해 바인딩 작업 필요
-    // 바인딩하지 않는 경우 this -> undefined 를 가리키게 된다.
-    // 메소드 생성 시마다 바인딩을 해줘야 하기 때문에 바벨의 transform-class-properties 문법을 사용
-    // 화살표 함수 형태로 메서드 형태를 정의하면 바인딩 작업 필요X
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      // 객체 안에서 key 를 [] 로 감싸면 그 안의 레퍼런스가 가리키는 실제 값이 key 값이 된다.
+    message: '',
+  });
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
       [e.target.name]: e.target.value,
-    });
+    };
+    setForm(nextForm);
   };
 
-  handleClick = () => {
-    alert(this.state.username + ' / ' + this.state.message);
-    this.setState({
+  const onClick = () => {
+    setForm({
       username: '',
       message: '',
     });
   };
 
-  handleKeyPress = (e) => {
+  const onKeyPress = (e) => {
     if (e.key === 'Enter') {
-      this.handleClick();
+      onClick();
     }
   };
 
-  render() {
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input
-          type="text"
-          name="username"
-          placeholder="사용자명"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="text 입력"
-          value={this.state.message}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
-        />
-        <button onClick={this.handleClick}>초기화</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="메세지"
+        value={message}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>초기화</button>
+    </div>
+  );
+};
 
 export default EventPractice;
