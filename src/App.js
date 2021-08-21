4967995193488fad6cd4a3e3a5ec2c355661c7a3
joohongkim1 <wnghd9938@naver.com>
2021-08-21 22:23:01 +0900

    component lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Counter from './Counter';
+import ErrorBoundary from './ErrorBoundary';
 import EventPractice from './EventPractice';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
 import MyComponent from './MyComponent';
 import Say from './Say';
 import ScrollBox from './ScrollBox';
 import ValidationSample from './ValidationSample';
 
-const App = () => {
-  return (
-    <div>
-      {/* <MyComponent favoriteNumber={1}>Children</MyComponent>
-      <Counter />
-      <Say /> */}
-      <EventPractice />
-      <ValidationSample />
-      <IterationSample />
-    </div>
-  );
-};
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <MyComponent favoriteNumber={1}>Children</MyComponent>
+//       <Counter />
+//       <Say /> */}
+//       {/* <EventPractice />
+//       <ValidationSample />
+//       <IterationSample /> */}
+//       <button></button>
+//     </div>
+//   );
+// };
+
+class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
 
 export default App;
